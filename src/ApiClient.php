a94d3619debab6c6c7c75c6e4b6f6910b39c91bf
ameijer <a.meijer@ccvshop.nl>
2023-05-10 13:03:47 +0200

    Config support via env files

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -8,20 +8,17 @@ use CCVShop\Api\Resources\Merchant;
 class ApiClient
 {
 	public \CCVShop\Api\ApiCredentials $apiCredentials;
-
 	public \CCVShop\Api\Endpoints\Credentials $credentials;
 	public \CCVShop\Api\Endpoints\Merchant $merchant;
 	public \CCVShop\Api\Endpoints\Webshops $webshops;
 
-	public function __construct(?string $hostName, ?string $apiPublic, ?string $apiSecret)
+	public function __construct(?string $hostName = null, ?string $apiPublic = null, ?string $apiSecret = null)
 	{
 		$this->apiCredentials = new \CCVShop\Api\ApiCredentials(
 			$hostName ?? $_ENV['CCVSHOP_API_HOSTNAME'],
 			$apiPublic ?? $_ENV['CCVSHOP_API_PUBLIC'],
 			$apiSecret ?? $_ENV['CCVSHOP_API_SECRET']
 		);
-
-
 
 		$this->credentials = new \CCVShop\Api\Endpoints\Credentials($this);
 		$this->merchant    = new \CCVShop\Api\Endpoints\Merchant($this);
