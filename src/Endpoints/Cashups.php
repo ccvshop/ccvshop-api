0a3fa3a614591e47591fe26f9d585cbae14bc03a
nvanenckevort <n.vanenckevort@ccvshop.nl>
2024-03-26 14:19:29 +0100

    mag er weer bij

---
+++ src/Endpoints/Cashups.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace CCVShop\Api\Endpoints;
+
+use CCVShop\Api\BaseEndpoint;
+use CCVShop\Api\Exceptions\InvalidHashOnResult;
+use CCVShop\Api\Interfaces\Endpoints\Get;
+use CCVShop\Api\Interfaces\Endpoints\GetAll;
+use CCVShop\Api\Resources\Cashup;
+use CCVShop\Api\Resources\CashupCollection;
+
+class Cashups extends BaseEndpoint implements
+    Get,
+    GetAll
+{
+    protected string $resourcePath = 'cashups';
+
+    /**
+     * @description Get the resource object
+     * @return Cashup;
+     */
+    protected function getResourceObject(): Cashup
+    {
+        return new Cashup($this->client);
+    }
+
+    /**
+     * @description Get the resource collection object
+     * @return CashupCollection
+     */
+    protected function getResourceCollectionObject(): CashupCollection
+    {
+        return new CashupCollection();
+    }
+
+    /**
+     * @description Get one by id
+     * @param int $id
+     * @return Cashup
+     * @throws InvalidHashOnResult
+     * @throws \CCVShop\Api\Exceptions\InvalidResponseException
+     * @throws \JsonException
+     */
+    public function get(int $id): Cashup
+    {
+        /** @var Cashup $result */
+        $result = $this->rest_getOne($id, []);
+
+        return $result;
+    }
+
+
+    /**
+     * @description Get all by parameters
+     * @param array $parameters
+     * @return CashupCollection
+     * @throws InvalidHashOnResult
+     * @throws \CCVShop\Api\Exceptions\InvalidResponseException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws \JsonException
+     */
+    public function getAll(array $parameters = []): CashupCollection
+    {
+        /** @var CashupCollection $result */
+        $result = $this->rest_getAll(null, null, $parameters);
+
+        return $result;
+    }
+}
