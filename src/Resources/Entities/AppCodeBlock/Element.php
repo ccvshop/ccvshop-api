750cdeeec3f9dddab4b6e96615e684e7ac356d7e
nvanenckevort <n.vanenckevort@ccvshop.nl>
2024-04-22 14:17:03 +0200

    duidelijke uitleg van wat welke property inhoud

--- src/Resources/Entities/AppCodeBlock/Element.php
+++ src/Resources/Entities/AppCodeBlock/Element.php
@@ -4,6 +4,20 @@ namespace CCVShop\Api\Resources\Entities\AppCodeBlock;
 
 use CCVShop\Api\Resources\Entities\AppCodeBlock;
 
+/**
+ * @description A single app code block form element.
+ * Multiple elements build up the view creating a form. An element can be static content of an input
+ * field. These elements are styled by CCV Shop to give the impression of a seamless integration into
+ * the CCV Shop environment. The properties within an element are:
+ * • Name: unique identifier of this element. Will be used as key when posting data.
+ * • Label: for input fields this will be a label for this element.
+ * • element_type: The type of element, e.g. an "text" input.
+ * • Value: the initial value of this element.
+ * • deeplink: Only used for deeplinks and attachments. Should contain the remote link to the file.
+ * • icon: Elements with the type ‘input_button’ and ‘deeplink’ may use the property ‘icon’. We support all icons from the icon set ‘font-awesome’4.
+ * • action: An action can be assigned to an `input_button`.
+ * • Options: Additional options for this element. Used for selects and radio elements.
+ */
 class Element extends AppCodeBlock
 {
     public ?string $name = null;
