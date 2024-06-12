06925f7c11266cb3fc5ec80f93d790a432ba3549
jschreurs <j.schreurs@ccvshop.nl>
2024-06-12 10:44:26 +0200

    St00pid bestand vergeten

---
+++ src/Resources/Entities/Label/Items.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CCVShop\Api\Resources\Entities\Label;
+
+use CCVShop\Api\Resources\Entities\Label;
+
+class Items extends ProductLabel
+{
+
+    public ?int $label_id = null;
+    public ?string $parent = null;
+
+}
