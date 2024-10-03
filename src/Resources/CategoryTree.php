3b252710b83522d1c9416da50d26938e4c15018b
npostma <n.postma@ccvshop.nl>
2024-10-03 11:08:33 +0200

    Fixes so that tree works

--- src/Resources/CategoryTree.php
+++ src/Resources/CategoryTree.php
@@ -7,13 +7,8 @@ use CCVShop\Api\BaseResource;
 class CategoryTree extends BaseResource
 {
     //SONAR_IGNORE_START
-    public ?int    $id              = null; // Unique id of the resource
-    public ?string $name            = null; // Category name (Maxlength: 255)
-    public ?string $description     = null; // Category description (Maxlength: 65536)
-    public ?int    $position        = null; // Category position (Minimum: 0)
-    public ?bool   $show_on_website = null; // Category visible on website
-    public ?string $deeplink        = null; // Deeplink to this resource
-    public ?array  $children        = [];   // Array with child categories (recursive)
+    public string  $href            = '';   // Link to self (Format: URI)
+    public array   $root_categories = [];   // Array with root categories
     //SONAR_IGNORE_END
 
     public array $permissions = [];
