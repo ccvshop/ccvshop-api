feb7a3507504940588e27c9b4087a36fa353b86e
npostma <n.postma@ccvshop.nl>
2024-10-03 09:13:50 +0200

    GetAll for categories

--- src/Endpoints/Categories.php
+++ src/Endpoints/Categories.php
@@ -3,16 +3,22 @@
 namespace CCVShop\Api\Endpoints;
 
 use CCVShop\Api\BaseEndpoint;
+use CCVShop\Api\BaseResource;
+use CCVShop\Api\BaseResourceCollection;
 use CCVShop\Api\Exceptions\InvalidHashOnResult;
+use CCVShop\Api\Exceptions\InvalidResponseException;
 use CCVShop\Api\Interfaces\Endpoints\Get;
 use CCVShop\Api\Interfaces\Endpoints\GetAll;
 use CCVShop\Api\Interfaces\Endpoints\Patch;
-use CCVShop\Api\Resources\AppConfig;
 use CCVShop\Api\Resources\Category;
 use CCVShop\Api\Resources\CategoryCollection;
+use GuzzleHttp\Exception\GuzzleException;
+use InvalidArgumentException;
+use JsonException;
 
 class Categories extends BaseEndpoint implements
     Get,
+    GetAll,
     Patch
 {
     protected string $resourcePath = 'categories';
@@ -32,41 +38,45 @@ class Categories extends BaseEndpoint implements
 
     /**
      * @param int $id
-     * @return Category
+     * @return BaseResource
      * @throws InvalidHashOnResult
-     * @throws \CCVShop\Api\Exceptions\InvalidResponseException
-     * @throws \JsonException
+     * @throws InvalidResponseException
+     * @throws JsonException
      */
-    public function get(int $id): Category
+    public function get(int $id): BaseResource
     {
         /** @var Category $result */
         return $this->rest_getOne($id, []);
     }
 
     /**
-     * @param Category|null $product
+     * @param Category|null $category
      * @return void
      * @throws InvalidHashOnResult
-     * @throws \CCVShop\Api\Exceptions\InvalidResponseException
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     * @throws \JsonException
+     * @throws InvalidResponseException
+     * @throws JsonException
      */
     public function patch(?Category $category = null): void
     {
         if (is_null($category)) {
-            throw new \InvalidArgumentException(Category::class . ' required');
+            throw new InvalidArgumentException(Category::class . ' required');
         }
 
-        /** @var AppConfig */
         $this->rest_patch($category->id, [
-            'name'                  => $category->name,
-            'description'           => $category->description,
-            'description_bottom'    => $category->description_bottom,
-            'searchwords'           => $category->searchwords,
-            'meta_description'      => $category->meta_description,
-            'meta_keywords'         => $category->meta_keywords,
-            'page_title'            => $category->page_title,
-            'alias'                 => $category->alias,
+            'name'               => $category->name,
+            'description'        => $category->description,
+            'description_bottom' => $category->description_bottom,
+            'searchwords'        => $category->searchwords,
+            'meta_description'   => $category->meta_description,
+            'meta_keywords'      => $category->meta_keywords,
+            'page_title'         => $category->page_title,
+            'alias'              => $category->alias,
         ]);
+    }
+
+    public function getAll(array $parameters = []): BaseResourceCollection
+    {
+        /** @var BaseResourceCollection $result */
+        return $this->rest_getAll(null, null, $parameters);
     }
 }
