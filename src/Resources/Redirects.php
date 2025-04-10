ff9f5585c5de86b349cff7c240de1fda058e6c6b
jwschreurs <jw_schreurs@hotmail.com>
2025-04-10 15:02:24 +0200

    incorrect file
    incorrect file

--- src/Resources/Redirects.php
+++ src/Resources/Redirects.php
@@ -1,137 +1,26 @@
 <?php
 
-namespace CCVShop\Api\Endpoints;
+    namespace CCVShop\Api\Resources;
 
-use CCVShop\Api\BaseEndpoint;
-use CCVShop\Api\BaseResource;
-use CCVShop\Api\Exceptions\InvalidHashOnResult;
-use CCVShop\Api\Exceptions\InvalidResponseException;
-use CCVShop\Api\Interfaces\Endpoints\Delete;
-use CCVShop\Api\Interfaces\Endpoints\Get;
-use CCVShop\Api\Interfaces\Endpoints\GetAll;
-use CCVShop\Api\Interfaces\Endpoints\Patch;
-use CCVShop\Api\Interfaces\Endpoints\Post;
-use CCVShop\Api\Resources\Redirect;
-use CCVShop\Api\Resources\RedirectCollection;
-use InvalidArgumentException;
-use JsonException;
-use ReflectionException;
+    use CCVShop\Api\BaseResource;
+    use CCVShop\Api\Endpoints\Redirects;
 
-class Redirects extends BaseEndpoint implements
-    Get,
-    GetAll,
-    Patch,
-    Post,
-    Delete
-{
-    protected string $resourcePath = 'redirects';
-
-    /**
-     * @return Redirect()
-     */
-    protected function getResourceObject(): Redirect
+    class Redirect extends BaseResource
     {
-        return new Redirect($this->client);
-    }
+        //SONAR_IGNORE_START
+        public ?int $id = null; // Unique id of the resource
 
-    /**
-     * @return RedirectCollection
-     */
-    protected function getResourceCollectionObject(): RedirectCollection
-    {
-        return new RedirectCollection();
-    }
+        public ?string $href = null;
+        public ?string $source_url = null; // Relative source
+        public ?string $target_url = null; // Relative or absolute target_url
+        public ?bool $active = null; // Is redirect active
 
-    /**
-     * @param int $id
-     * @return BaseResource
-     * @throws InvalidHashOnResult
-     * @throws InvalidResponseException
-     * @throws JsonException|ReflectionException
-     */
-    public function get(int $id): BaseResource
-    {
-        /** @var Redirect $result */
-        return $this->rest_getOne($id, []);
-    }
+        public ?object $parent = null; // Parent resource of this resource
+        //SONAR_IGNORE_END
 
-    /**
-     * @param array $parameters
-     * @return RedirectCollection
-     * @throws InvalidHashOnResult
-     * @throws InvalidResponseException
-     * @throws JsonException|ReflectionException
-     */
-    public function getAll(array $parameters = []): RedirectCollection
-    {
-        /** @var RedirectCollection */
-        return $this->rest_getAll(null, null, $parameters);
-    }
 
-    /**
-     * @param Redirect|null $redirect
-     * @return void
-     * @throws InvalidHashOnResult
-     * @throws InvalidResponseException
-     * @throws JsonException
-     */
-    public function patch(?Redirect $redirect = null): void
-    {
-        if (is_null($redirect)) {
-            throw new InvalidArgumentException(Redirect::class . ' required');
+        public function getEndpoint(): Redirects
+        {
+            return $this->client->redirects;
         }
-
-        $data = [
-            'source_url' => $redirect->source_url,
-            'target_url' => $redirect->target_url,
-            'active'     => $redirect->active,
-        ];
-
-        // Filter the array to remove entries with null values
-        $data = array_filter($data, function ($value) {
-            return !is_null($value);
-        });
-
-        $this->rest_patch($redirect->id, $data);
     }
-
-    /**
-     * @param Redirect|null $redirect
-     * @return Redirect
-     * @throws InvalidHashOnResult
-     * @throws InvalidResponseException
-     * @throws JsonException
-     * @throws ReflectionException
-     */
-    public function post(?Redirect $redirect = null): Redirect
-    {
-        if (is_null($redirect)) {
-            throw new InvalidArgumentException(Redirect::class . ' required');
-        }
-
-        $data = [
-            'source_url' => $redirect->source_url,
-            'target_url' => $redirect->target_url,
-            'active'     => $redirect->active,
-        ];
-
-        // Filter the array to remove entries with null values
-        $data = array_filter($data, function ($value) {
-            return !is_null($value);
-        });
-
-        return $this->rest_post($data);
-    }
-
-    /**
-     * @param int $id
-     * @return void
-     * @throws InvalidHashOnResult
-     * @throws InvalidResponseException
-     * @throws JsonException
-     */
-    public function delete(int $id): void
-    {
-        $this->rest_delete($id);
-    }
-}
