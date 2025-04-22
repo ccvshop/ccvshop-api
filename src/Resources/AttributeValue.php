abd8eac3614da88171404a6fa3d7cdfa32500e15
eerdem <e.erdem@ccvshop.nl>
2025-04-22 10:02:57 +0200

    updated attribute resources with ignore sonar comment

--- src/Resources/AttributeValue.php
+++ src/Resources/AttributeValue.php
@@ -7,12 +7,18 @@ use CCVShop\Api\Endpoints\AttributeValues;
 
 class AttributeValue extends BaseResource
 {
+    //SONAR_IGNORE_START
+    // This needs to be ignored to match the API structure exactly.
     public ?int $id               = null;
-    public ?int $attribute_id     = null;
-    public ?string $name          = null;
-    public ?string $default_price = null;
-    public ?string $type          = null;
 
+    public ?int $attribute_id     = null;
+
+    public ?string $name          = null;
+
+    public ?string $default_price = null;
+
+    public ?string $type          = null;
+    //SONAR_IGNORE_END
 
     /**
      * @return AttributeValues
--- src/Resources/ProductAttributeValue.php
+++ src/Resources/ProductAttributeValue.php
@@ -8,14 +8,24 @@ use stdClass;
 
 class ProductAttributeValue extends BaseResource
 {
+    //SONAR_IGNORE_START
+    // This needs to be ignored to match the API structure exactly.
     public ?int    $id               = null;
+
     public ?int    $optionid         = null;
+
     public ?int    $optionvalue_id   = null;
+
     public ?string $optionvalue_name = null;
+
     public ?string $optionname       = null;
+
     public ?int    $optionposition   = null;
+
     public ?int    $price            = null;
+
     public ?stdClass    $parent            = null;
+    //SONAR_IGNORE_END
 
 
 
